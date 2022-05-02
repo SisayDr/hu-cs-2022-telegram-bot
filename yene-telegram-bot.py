8e159ff1f776c7ebdeea56554c70d5c69111a3ab
Sisay Dr <99421721+SisayDr@users.noreply.github.com>
2022-05-02 19:06:31 +0300

    Update yene-telegram-bot.py

--- yene-telegram-bot.py
+++ yene-telegram-bot.py
@@ -1,9 +1,11 @@
 import os, telebot, csv
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
 
+#API_KEY = '5376008511:AAF0PUs25QGnIfmf0cFcU53O3tnS3e1lgPk'
 API_KEY = '5114435234:AAGyFATaFCUXyonmRV8kB68WGTBP-s2oDOc'
+
 bot = telebot.TeleBot(API_KEY)
-bot.set_webhook()
+#bot.set_webhook()
 
 tg_std_ids = {616540060: '0102/13', 1085350448: '0406/13', 1457760659: '4748/13', 716072962: '0547/13', 1852599241: '0571/13', 790361728: '0727/13', 946954073: '0793/13', 5275555010: '0857/13', 1749942403: '0987/13', 5124247505: '1092/13', 1326369761: '1178/13', 642540516: '1198/13', 1650564210: '1247/13', 659006861: '1380/13', 1607004128: '1485/13', 5243708594: '1496/13', 835007431: '1501/13', 1869220319: '1542/13', 687140116: '1630/13', 1671602737: '1651/13', 1352447923: '1653/13', 781655041: '1970/13', 1429824442: '1974/13', 788617323: '2006/13', 1601244847: '2050/13', 1935617579: '2063/13', 196262476: '2082/13', 1122447230: '2134/13', 1165764727: '2357/13', 5058453464: '2494/13', 705112575: '2505/13', 476765725: '2521/13', 1486466727: '2525/13', 1067019910: '2550/13', 1288185971: '2723/13', 1910231449: '2726/13', 1788848778: '2799/13', 1651407207: '2878/13', 1822882222: '2956/13'}
 
@@ -12,37 +14,6 @@ def get_tg_id(student_id):
     if student_id == std_id:
       return tg_id
 
-def send_grade_and_ranks():
-  with open("./Assets/CS-TG.csv", "r") as cs_data:
-    reader = csv.DictReader(cs_data)
-    for row in reader:
-      tg_id = row["Telegram ID"]
-      if float(row["Cumulative"]) > float(row["21 CGPA"]):
-        comment = "  Your Grade seems to be going down. watch out!"
-        if float(row["Cumulative"]) > 3:
-          comment += "\n Your're still doing good 👍"
-      elif float(row["21 CGPA"]) >= float(row["Cumulative"]):
-        if float(row["Cumulative"]) > 3.5:
-          comment = "  👏👏👏\n  You're doing great. Keep it up!"
-        else:
-          comment = "  You did great. But, you still need to lift your grade up.\n  *Good job anyways* 👏👏👏"
-
-
-      msg = row["First Name"]+" "+row["Middle Name"]+'''
-    --------------------------------------------------
-                               | CGPA |  Rank  |
-    --------------------------------------------------
-    First Semester | '''+row["21 CGPA"]+"    |   "+row["21 Rank"]+'''       |
-    --------------------------------------------------
-          Cumulative | '''+row["Cumulative"]+"    |   "+row["Rank"]+ '''       |
-    --------------------------------------------------'''
-
-      try:
-        bot.send_message(tg_id, msg)
-        bot.send_message(tg_id, comment)
-      except:
-        print(tg_id, row["First Name"]+" "+row["Middle Name"])
-        continue
 
 
 def send_result():
